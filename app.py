e7e7bd27da2e2df8de4230da86a313c8e4f6263b
Brandon Sov <brandonsov@gmail.com>
2016-10-15 11:41:22 -0700

    deleted facebook related api code

--- app.py
+++ app.py
@@ -5,9 +5,5 @@ app = Flask(__name__)
 def hello():
     return flask.render_template('fbsignin.html')
 
-# @app.route("/fb_signin")
-# def sign_in():
-#     return flask.render_template('fbsignin.html')
-
 if __name__ == "__main__":
     app.run()
