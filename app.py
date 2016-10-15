ce698b3c63e1d2b0773c98e13ab0cb01169f4d2f
Brandon Sov <brandonsov@gmail.com>
2016-10-15 10:03:28 -0700

    changed route

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
-
-@app.route("/fb_signin")
-def sign_in():
     return flask.render_template('fbsignin.html')
+
+# @app.route("/fb_signin")
+# def sign_in():
+#     return flask.render_template('fbsignin.html')
 
 if __name__ == "__main__":
     app.run()
